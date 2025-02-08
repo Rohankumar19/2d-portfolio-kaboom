dca8eec5a9bc36b1d8a2afeeb2a8689b6d5fce95
Rohan Kumar <rohankumar19003@gmail.com>
2025-02-08 09:55:06 +0530

    init

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Solidity and C++. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Rohankumar19" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`, 
+
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1YK4WF979u9cUUsmAQQZb8xGCauOt5BgJ/view?usp=drive_link" target="_blank">Check it out?</a>
+  Contact me at rohankumar19003@gmail.com if you have any interesting job opportunities!`,
+  projects: `I built a 2D metaverse game like Gather Town, focusing on WebSockets, API design, and a mono-repo structure. The project involved Kubernetes with GitOps, CI/CD pipelines, and discussions on database placement. Eventually, I migrated the backend to Golang for better performance and scalability.<a href="https://github.com/Rohankumar19/2d-metaverse-main" target="_blank">here</a>.`,
+  library: `Here are some of my project works that I have done: 
+  Multithreaded-Proxy-Web-Server <a href="https://github.com/Rohankumar19/Multithreaded-Proxy-Web-Server" target="_blank">here</a>, 
+  web3-saas <a href="https://github.com/Rohankumar19/web3-saas" target="_blank">here</a>, 
+  and Figo <a href="https://figo-1.netlify.app/" target="_blank">here</a>.`,
+  exit: `If you want to exit rohan's portfolio, just close the tab.`,
+};
